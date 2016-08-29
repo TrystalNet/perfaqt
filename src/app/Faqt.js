aad2796d59e68da2d60823b745bc1bcb59d9235c
Terry O'Neill <terry.oneill@trystal.net>
2016-08-29 07:23:32 -0600

    more code cleanup

--- src/app/Faqt.js
+++ src/app/Faqt.js
@@ -13,13 +13,23 @@ const styleInactive = {
   flex:1,
   minWidth:0,
   overflowX: 'auto',
-  marginRight: 5
+  marginRight: 5,
+  whiteSpace:'pre-wrap',
+  wordWrap:'break-word'
 }
 
 const styleTextareaActive = {
   border: 'black 0px solid',
   backgroundColor: 'beige',
   fontSize:14
+}
+
+const styleEditorContainer = {
+  backgroundColor:'beige', 
+  paddingTop:5, 
+  paddingBottom:5, 
+  marginBottom:4,
+  marginRight: 48
 }
 
 class Faqt extends Component {
@@ -44,22 +54,16 @@ class Faqt extends Component {
     this.props.onDeactivate()
   }
   render() {
-    let fldText
     const {id, text, isActive, onSetBest, onChange, onActivate, onDeactivate} = this.props
     if(!isActive) {
-      const bits = text.split(/\r?\n/)
-      return (
-        <div style={styleContainerInactive}>
-          <div style={styleInactive} onClick={onActivate}>
-            {
-              bits.map((bit,index) => bit ? <div key={index}>{bit}</div> : <div key={index}>&nbsp;</div>)
-            }
-          </div>
-          <button onClick={onSetBest}>best</button>
-        </div>
-      )
+      return <div style={styleContainerInactive}>
+        <div style={styleInactive} onClick={onActivate}>{text}</div>
+        <button onClick={onSetBest}>best</button>
+      </div>
     }
-    return <div style={{backgroundColor:'beige', paddingTop:5, paddingBottom:5, marginBottom:4}}><MyEditor {...{text}} onChanged={this.onDraftChanged.bind(this)}/></div>
+    return <div style={styleEditorContainer}>
+      <MyEditor {...{text}} onChanged={this.onDraftChanged.bind(this)}/>
+    </div>
   }  
 }
 
